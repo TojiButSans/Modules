44cde943fb792058ff0c720045f41bce8847df5e
TojiButSans <daiblo15techs@gmail.com>
2025-08-21 14:46:58 +0300

    Update and rename Module1.lua to MainModule.lua

---
+++ MainModule.lua
@@ -0,0 +1,10 @@
+local module = {}
+repeat wait() until game:IsLoaded()
+function module:BringItem(type, items)
+  if type == item then
+    for _, v in pairs(items:GetChildren())
+     print("uncompleted")
+    end
+  end
+end
+return module
