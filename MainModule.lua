d133f231f84a4d5c1cc20e248179ca7c989e1135
TojiButSans <daiblo15techs@gmail.com>
2025-08-21 15:02:17 +0300

    Update MainModule.lua

--- MainModule.lua
+++ MainModule.lua
@@ -1,10 +1,11 @@
 local module = {}
+local victims = {}
+local chars = workspace.Characters
+local items = workspace.Items
 repeat wait() until game:IsLoaded()
-function module:BringItem(type, items)
-  if type == item then
-    for _, v in pairs(items:GetChildren())
-     print("uncompleted")
-    end
+function module:KillAura(dis, mob)
+  for _, v in pairs(chars:GetChildren())
+    if (humRP.Posisition - v.HumanoidRootPart.Position).Magnitude <= dis then
   end
 end
 return module
