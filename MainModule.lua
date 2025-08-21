e5122c41ee1bc3813860754dc30e3117e5fac606
TojiButSans <daiblo15techs@gmail.com>
2025-08-21 15:32:26 +0300

    Update MainModule.lua

--- MainModule.lua
+++ MainModule.lua
@@ -1,14 +1,39 @@
 local module = {}
 local vics = {}
+local p1 = game.Players.LocalPlayer
+local wepdmgIds = {
+    ["Old Axe"] = "1_" .. p1.UserId,
+    ["Good Axe"] = "112_" .. p1.UserId,
+    ["Strong Axe"] = "116_" .. p1.UserId,
+    ["Chainsaw"] = "647_" .. p1.UserId,
+    ["Spear"] = "196_" .. p1.UserId
+}
 local chars = workspace.Characters
 local items = workspace.Items
+local RE = ReplicatedStorage:WaitForChild("RemoteEvents")
 repeat wait() until game:IsLoaded()
-function module:KillAura(dis, mob, wep)
+function module:CheckWep()
+    for i, v in pairs(wepdmgIds) do
+        local wepon = p1.Inventory:FindFirstChild(i)
+        if wepon then
+            return wep, dmgId
+        end
+    end
+    return nil, nil
+end
+function module:KillAura(dis)
   for _, v in pairs(chars:GetChildren())
     if (humRP.Posisition - v.HumanoidRootPart.Position).Magnitude <= dis then
-game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("ToolDamageObject"):InvokeServer(workspace:WaitForChild("Characters"):WaitForChild("Bunny"),
-	game:GetService("Players").LocalPlayer:WaitForChild("Inventory"):WaitForChild("Old Axe"),
-	"0_".. game.Player.UserId)
+	  local wep, dmgId = module:CheckWep()
+	  table.insert(vics, v)
+      pcall(function()
+	    RE.ToolDamageObject:InvokeServer(
+         vics,
+         wep,
+         dmgId,
+         CFrame.new(v.HumanoidRootPart.CFrame)
+	    )
+	  end)
     end
   end
 end
