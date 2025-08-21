7a97526cb0be02a3a6b88bdb9f7efd46c4060a86
TojiButSans <daiblo15techs@gmail.com>
2025-08-21 15:06:56 +0300

    Update MainModule.lua

--- MainModule.lua
+++ MainModule.lua
@@ -1,11 +1,15 @@
 local module = {}
-local victims = {}
+local vics = {}
 local chars = workspace.Characters
 local items = workspace.Items
 repeat wait() until game:IsLoaded()
-function module:KillAura(dis, mob)
+function module:KillAura(dis, mob, wep)
   for _, v in pairs(chars:GetChildren())
     if (humRP.Posisition - v.HumanoidRootPart.Position).Magnitude <= dis then
+game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("ToolDamageObject"):InvokeServer(workspace:WaitForChild("Characters"):WaitForChild("Bunny"),
+	game:GetService("Players").LocalPlayer:WaitForChild("Inventory"):WaitForChild("Old Axe"),
+	"0_".. game.Player.UserId)
+    end
   end
 end
 return module
