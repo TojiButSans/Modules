62a0c077cbf72dead8078a9a25212b356bef2a0f
TojiButSans <daiblo15techs@gmail.com>
2025-09-06 00:36:14 +0300

    Update SchlapModule.lua

--- MainModule.lua
+++ MainModule.lua
@@ -1,1 +1,14 @@
-local funs = {}
+local Utility = {}
+-- Variables
+local plr = game.Players.LocalPlayer
+-- functions
+function Utility:GetAllPlayers()
+    local chars = {}
+    for _, v in ipairs(game.Players:GetPlayers()) do
+        if v ~= plr then
+            table.insert(chars, v.Character)
+        end
+    end
+    return chars
+end
+return Utitlity
