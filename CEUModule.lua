ca4a246bcae632eeda9234fb8dd64ab106e4e894
TojiButSans <daiblo15techs@gmail.com>
2025-09-11 01:47:45 +0300

    Update CEUModule.lua

--- CEUModule.lua
+++ CEUModule.lua
@@ -5,6 +5,9 @@ local plr = game.Players.LocalPlayer
 local regs = workspace:WaitForChild("Regions")
 
 -- Functions
+function Utility:FireRemote(REpath, REargs)
+  REpath::FireServer(unpack(REargs))
+end
 function Utility:GetAllTiles()
 	local tiles = {}
 	for _, v in ipairs(regs:GetChildren()) do
