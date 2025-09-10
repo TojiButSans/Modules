49087a757c4c2854c81e620bab1b96714e166591
TojiButSans <daiblo15techs@gmail.com>
2025-09-11 01:45:18 +0300

    Update CEUModule.lua

--- CEUModule.lua
+++ CEUModule.lua
@@ -1,17 +1,20 @@
 local Utility = {}
+
 -- Variables
 local plr = game.Players.LocalPlayer
-local regs = workspace.Regions
--- functions
+local regs = workspace:WaitForChild("Regions")
+
+-- Functions
 function Utility:GetAllTiles()
-  local tiles = {}
-  for _, v in pairs(regs:GetChildren()) do
-    if v:IsA("Folder") then
-      if v:GetAttribute("Country", plr:GetAttribute("MyCountry"))
-        table.insert(tiles, v)
-      end
-    end
-  end
-  return tiles
+	local tiles = {}
+	for _, v in ipairs(regs:GetChildren()) do
+		if v:IsA("Folder") then
+			if v:GetAttribute("Core") == plr:GetAttribute("MyCountry") then
+				table.insert(tiles, v)
+			end
+		end
+	end
+	return tiles
 end
+
 return Utility
